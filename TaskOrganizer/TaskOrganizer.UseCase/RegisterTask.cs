62182031f4934771f63a0c5f4529c482a1e0a923
Anderson <gcosta19@gmail.com>
2020-03-05 00:38:10 -0300

    Created class RegisterTask

---
+++ TaskOrganizer/TaskOrganizer.UseCase/RegisterTask.cs
@@ -0,0 +1,18 @@
+﻿using TaskOrganizer.Domain.Entities;
+using TaskOrganizer.UseCase.ContractUseCase;
+
+namespace TaskOrganizer.UseCase
+{
+    class RegisterTask : IRegisterTask
+    {
+        // rules***
+        // there must be a title
+        // there must be a descripiton
+        // there must be a estimeted date - case don't exists this the system will put 30 days automatically
+        // the create date will put automatically when the task is registered
+        public void Register(DomainTask domainTask)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

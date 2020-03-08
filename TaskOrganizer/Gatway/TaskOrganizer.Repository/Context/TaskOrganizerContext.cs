466804529b28ee43d5d0c7ca599e3e73dd0ac68e
Anderson <gcosta19@gmail.com>
2020-03-08 20:42:09 -0300

    dotnet version changed to 3.0

--- TaskOrganizer/Gatway/TaskOrganizer.Repository/Context/TaskOrganizerContext.cs
+++ TaskOrganizer/Gatway/TaskOrganizer.Repository/Context/TaskOrganizerContext.cs
@@ -1,8 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace TaskOrganizer.Repository.Context
 {
-    public class TaskOrganizerContext : DbContext
+    public class TaskOrganizerContext 
     {
         
     }

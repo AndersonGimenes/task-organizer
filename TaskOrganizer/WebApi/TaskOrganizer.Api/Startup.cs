995b3f6d81570391795c131da109dae4bd5a12e9
Anderson <gcosta19@gmail.com>
2020-03-08 23:34:05 -0300

    Create database connection and configuration of repositories

--- TaskOrganizer/WebApi/TaskOrganizer.Api/Startup.cs
+++ TaskOrganizer/WebApi/TaskOrganizer.Api/Startup.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TaskOrganizer.Repository.Context;
 
 namespace TaskOrganizer.Api
 {
@@ -26,10 +22,14 @@ namespace TaskOrganizer.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<TaskOrganizerContext>(
+                option => option
+                    .UseMySql(Configuration.GetConnectionString("Default"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
